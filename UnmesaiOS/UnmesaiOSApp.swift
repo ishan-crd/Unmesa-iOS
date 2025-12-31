727a0c51ffae5442faee4915ec93a1ef767049c1
ishan-crd <ishangupta3121@gmail.com>
2025-12-31 15:32:22 +0530

    Initial Commit

---
+++ UnmesaiOS/UnmesaiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnmesaiOSApp.swift
+//  UnmesaiOS
+//
+//  Created by Ishan Gupta on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct UnmesaiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
